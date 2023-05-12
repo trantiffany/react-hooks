1d9b65a5cc5b0c2254d334dba9771a60f63e4d68
ttran-bread <tiffany.tran@breadfinancial.com>
2023-05-12 13:42:54 -0500

    custom hooks ++

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import "./App.css";
-import { useInput } from "./useInput";
+import { useTrees } from "./";
+import { useFetch } from "./useFetch";
 
-function App() {
-  const [titleProps, resetTitle] = useInput("");
-  const [colorProps, resetColor] = useInput("#FFFFFF");
+function App({login}) {
+  const {loading, data, error} = useFetch(`https://api.github.com/users/${login}`)
+  
+  const { trees } = useTrees();
+  console.log(data)
+  if (loading) return <h1>loading...</h1>
+  if (error) return (
+    <pre>{JSON.stringify(error, null, 2)}</pre>
+  );
 
-  const submit = (e) => {
-    e.preventDefault();
-    alert(`${titleProps.value} sounds like ${colorProps.value}`);
-    resetTitle();
-    resetColor();
-  };
   return (
-    <form onSubmit={submit}> 
-      <input {...titleProps} type="text" placeholder="Title..." />
-      <input {...colorProps} type="color" />
-      <button>Add</button>
-    </form>
+    <div>
+      <h1>
+        Trees I've heard of
+      </h1>
+      <ul>
+        {trees.map((tree) => (
+          <li key={tree.id}>{tree.type}</li>
+        ))}
+      </ul>
+      <div>
+    </div>
+    <div>
+      <div>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <img src={data.avatar_url}/>
+      <h1>{data.name}</h1>
+      </div>
+    </div>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createContext, useContext } from "react";
 
+export const TreesContext = createContext();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const trees = [
+  { id: "1", type: "Maple" },
+  { id: "2", type: "Oak" },
+  { id: "3", type: "Maple" },
+];
+
+export const useTrees = () => useContext(TreesContext);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App name="Alex"/>
-  </React.StrictMode>
+  <TreesContext.Provider value={{ trees }}>
+    <App login="eveporcello" />
+  </TreesContext.Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
