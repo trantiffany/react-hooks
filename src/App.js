6236f4a4cd769c08a70a49944b19b887700a8a5b
ttran-bread <tiffany.tran@breadfinancial.com>
2023-05-12 11:23:45 -0500

    useReduce number

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useReducer } from "react";
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(()=>{
-    fetch(`https://api.github.com/users`)
-    .then(res => res.json())
-    .then(setData)
-  }, [])
-
-  if(data){
-    return(
-      <div>
-      <ul>
-        {data.map((user)=>(
-          <li key={user.id}>{user.login}</li>
-        ))}
-      </ul>
-      <button onClick={()=>setData([])}>
-          Remove Data
-      </button>
-      </div>
-    )
-  }
-  return (
-      <p>No Users</p>
+  const [number, setNumber] = useReducer(
+    (number, newNumber) => number + newNumber,
+    0
   );
+  return <h1 onClick={() => setNumber(1)}>{number}</h1>;
 }
 
 export default App;
