a3ea0f1c56c5cfa58c395b96fdc271b8b183bb93
ttran-bread <tiffany.tran@breadfinancial.com>
2023-05-12 11:49:46 -0500

    custom hook useInput

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
-import { useReducer } from "react";
+import { useInput } from "./useInput";
 
 function App() {
-  const [number, setNumber] = useReducer(
-    (number, newNumber) => number + newNumber,
-    0
+  const [titleProps, resetTitle] = useInput("");
+  const [colorProps, resetColor] = useInput("#FFFFFF");
+
+  const submit = (e) => {
+    e.preventDefault();
+    alert(`${titleProps.value} sounds like ${colorProps.value}`);
+    resetTitle();
+    resetColor();
+  };
+  return (
+    <form onSubmit={submit}> 
+      <input {...titleProps} type="text" placeholder="Title..." />
+      <input {...colorProps} type="color" />
+      <button>Add</button>
+    </form>
   );
-  return <h1 onClick={() => setNumber(1)}>{number}</h1>;
 }
 
 export default App;
